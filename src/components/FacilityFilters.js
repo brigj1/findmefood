82ec95c600576fd0a7abb29871821e6484ce571f
Brigham <brig.johnson@gmail.com>
2023-04-10 11:05:32 -0400

    r show Facility Filters via Table

--- src/components/FacilityFilters.js
+++ src/components/FacilityFilters.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useStyles } from "../hooks/useStyles";
+import Table from 'react-bootstrap/Table';
 
 const FacilityFilters = ({
   handleFoodItemsChange,
@@ -13,36 +15,67 @@ const FacilityFilters = ({
 
   initializeFilters
 }) => {
+    const { tableShow } = useStyles();
 
     return (
       <div>
         <br/>
-        <b>Filter by:</b>
-        <br/>Food Type: <input
-            name="selectedFoodItems"
-            value={selectedFoodItems}
-            onChange={handleFoodItemsChange}
-            placeholder="Type of food you'd like?"
-          />
-        <br/>Facility Type: <input
-            name="selectedFacilityType"
-            value={selectedFacilityType}
-            onChange={handleFacilityTypeChange}
-            placeholder="Truck or Push Cart?"
-          />
-        <br/>Applicant/Owner: <input
-            name="selectedApplicant"
-            value={selectedApplicant}
-            onChange={handleApplicantChange}
-            placeholder="Enter Owner Info"
-          />
-        <br/>Status: <input
-            name="selectedStatus"
-            value={selectedStatus}
-            onChange={handleStatusChange}
-            placeholder="App, Req, Exp, Susp?"
-          />
-        <br/><button onClick={initializeFilters}>Reset Filters</button>
+        <span className={`center`}>
+          <b>Search by:</b>
+        </span>
+        <br/>
+        <Table striped bordered hover style={tableShow}>
+          <thead>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Food Type:</td>
+              <td>
+                <input
+                  name="selectedFoodItems"
+                  value={selectedFoodItems}
+                  onChange={handleFoodItemsChange}
+                  placeholder="Type of food you'd like?"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Facility Type:</td>
+              <td>
+                <input
+                  name="selectedFacilityType"
+                  value={selectedFacilityType}
+                  onChange={handleFacilityTypeChange}
+                  placeholder="Truck or Push Cart?"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Applicant/Owner:</td>
+              <td>
+                <input
+                  name="selectedApplicant"
+                  value={selectedApplicant}
+                  onChange={handleApplicantChange}
+                  placeholder="Enter Owner Info"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Status:</td>
+              <td>
+                <input
+                  name="selectedStatus"
+                  value={selectedStatus}
+                  onChange={handleStatusChange}
+                  placeholder="App, Req, Exp, Susp?"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        <br/><button className={`center`} onClick={initializeFilters}>Reset Filters</button>
+        <br/>
       </div>
     )
 }
